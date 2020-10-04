24742ae3bad0e98581503dbf7fca05e7870fdfae
Harsh-khatriemail <18IT048@charusat.edu.in>
2020-10-05 00:28:07 +0530

    Second commit

---
+++ view.py
@@ -0,0 +1,9 @@
+from PyQt5.QtCore import Qt
+
+from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QGridLayout
+from PyQt5.QtWidgets import QLineEdit
+from PyQt5.QtWidgets import QPushButton
+from PyQt5.QtWidgets import QVBoxLayout
+
